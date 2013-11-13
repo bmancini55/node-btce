0c2d05502ea8325541f95523b863c0646b1b9ead
bmancini55 <bmancini@gmail.com>
2013-11-12 21:06:08 -0800

    Updated index for tradehelpers

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var PublicClient = require('./lib/publicclient')
-  , TradeClient = require('./lib/tradeclient');
+var PublicClient  = require('./lib/publicclient')
+  , TradeClient   = require('./lib/tradeclient')
+  , tradehelpers  = require('./lib/tradehelpers');
 
 function Btce() {}
 Btce.prototype.PublicClient = PublicClient;
 Btce.prototype.TradeClient = TradeClient;
-
+Btce.prototype.tradehelpers = tradehelpers;
 module.exports = new Btce();
